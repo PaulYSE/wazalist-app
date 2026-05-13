80412438312ebcdb20a7d6a69875c52f6121dbdb
Paul <yongpaul@rocketmail.com>
2026-05-13 16:43:43 +0800

    testing database connection

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { renderHtml } from "./renderHtml";
 
 export default {
 	async fetch(request, env) {
-		const stmt = env.DB.prepare("SELECT * FROM comments LIMIT 3");
+		const stmt = env.DB.prepare("SELECT * FROM waza LIMIT 20");
 		const { results } = await stmt.all();
 
 		const html = await renderHtml(env, JSON.stringify(results, null, 2));
